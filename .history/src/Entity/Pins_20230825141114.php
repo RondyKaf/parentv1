5550f81abe164a30eed73f28455b9d035a82c245
rondykaf <rondykfm@gmail.com>
2023-08-25 14:57:18 +0100

    add boostrap & use form themes flix little bug with setter

---
+++ .history/src/Entity/Pins_20230825141114.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Traits\Timestampables;
+use App\Repository\PinsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: PinsRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Pins
+{
+
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 3, minMessage: "doit contenir au moins 3 caractères")]
+
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 10, minMessage: "doit contenir au moins 10 caractères")]
+
+    private ?string $description = null;
+    use Timestampables;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+
+}
